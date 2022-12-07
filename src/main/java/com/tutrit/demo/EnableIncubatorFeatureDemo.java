83d060cb6446fe8ba046a59e43ad58feb263421e
mikas <6666350@gmail.com>
2022-12-07 23:10:18 +0100

    adds Enable Incubator Feature demo

---
+++ src/main/java/com/tutrit/demo/EnableIncubatorFeatureDemo.java
@@ -0,0 +1,10 @@
+package com.tutrit.demo;
+
+import jdk.incubator.vector.FloatVector;
+
+public class EnableIncubatorFeatureDemo {
+
+    public static void main(String[] args) {
+        System.out.println(FloatVector.SPECIES_PREFERRED);
+    }
+}
